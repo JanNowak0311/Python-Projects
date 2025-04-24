d238a4f77e4e10e5ceccf6f40ea691e5804c1a6f
JanNowak0311 <105235113+JanNowak0311@users.noreply.github.com>
2025-04-24 18:58:54 +0200

    Update File organizer.py

--- File organizer.py
+++ File organizer.py
@@ -1,16 +1,20 @@
 import os
 import shutil
-
-path = "your/folder/path"  # Change this to your folder path
-files = os.listdir(path)
-
-for file in files:
-    if file.endswith(".txt"):
-        if not os.path.exists("Text"):
-            os.makedirs("Text")
-        shutil.move(os.path.join(path, file), os.path.join(path, "Text", file))
-    elif file.endswith(".jpg"):
-        if not os.path.exists("Images"):
-            os.makedirs("Images")
-        shutil.move(os.path.join(path, file), os.path.join(path, "Images", file))
-print("Files organized")
+print("To exit the app just type in 'out'")
+while True:
+    path = input("Type in your folder path (e.g. 'your/folder/path')")
+    if path == "out":
+        print("Goodbye")
+        break
+    else:
+        files = os.listdir(path)
+        for file in files:
+            if file.endswith(".txt"):
+                if not os.path.exists("Text"):
+                    os.makedirs("Text")
+                shutil.move(os.path.join(path, file), os.path.join(path, "Text", file))
+            elif file.endswith(".jpg"):
+                if not os.path.exists("Images"):
+                    os.makedirs("Images")
+                shutil.move(os.path.join(path, file), os.path.join(path, "Images", file))
+        print("Files organized")
